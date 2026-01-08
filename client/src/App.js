7ddcc86b02a3daa3d170382b6406891bb7ed6b62
mohamm4c <85082155+mohamm4c@users.noreply.github.com>
2026-01-08 18:35:11 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ function App() {
           >
             Learn React
           </a>
-          <Link to="/">Home</Link>
+          <Link to="/">Another Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
